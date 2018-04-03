d2dd6964bbe5ae66845f8c598e7fa58c99b91499
Dian Baghawan <bp@mineral.co.id>
2018-04-03 15:25:54 +0700

    talk form

--- _html/src/js/global.js
+++ _html/src/js/global.js
@@ -1,10 +1,8 @@
 $(function(){
     //Init on all pages
 
-    // $('#hamburger').on('click',function(){
-    //     $(this).toggleClass('is-active');
-    // });
     rdm.ready();
+    rdm.talk();
 
     rdm.fullHeight('.fullheight-js');
     rdm.minHeight('.minheight-js');
@@ -198,20 +196,135 @@ var rdm = (function(){
             holder.removeClass('is-opened');
             detail.removeClass('is-hover is-active');
         })
-    }
+    };
+
+    var talk = function(){
+        var btn      = $('#trigger-talk'),
+            template = $('script[data-template="talk-modal"]').html();
+
+        function hideTalkModal(){
+            $('.js-slide').addClass('is-leaving');
+            $('#talk-modal').removeClass('anim-finish');
+            
+
+            setTimeout(function(){
+                $('#talk-modal').removeClass('active');
+            }, 20);
+
+            setTimeout(function(){
+                $('#talk-modal').remove();
+                $('html').removeClass('talk-active');
+            }, 1200);
+        };
+
+        function showTalkModal(){
+            $('html').addClass('talk-active');
+            $('body').append(template);
+
+            setTimeout(function(){
+                $('#talk-modal').addClass('active');
+                
+                setTimeout(function(){
+                    $('#talk-modal').addClass('anim-finish');
+                    $('.js-slide').first().addClass('is-active');
+                    $('input.form-control').focus();
+                }, 600);
+            }, 20);
+        };
+
+        btn.click(function(e){
+            e.preventDefault();
+
+            showTalkModal();
+
+            if($('body').find('#talk-modal').html().length > 0){
+                var field = $('.modal-field');
+
+                field.keypress(function(e){
+                    if(e.which == 13) {
+                        e.preventDefault();
+                        console.log('sdsd');
+                        nextInput();
+                    }
+                })
+
+                // next input field
+                $('#talk-modal').on('click', '.btn-next', function(e){
+                    e.preventDefault();
+                    nextInput();
+                });
+
+                function nextInput(){
+                    var item = $('.js-slide.is-active');
+                    item.removeClass('is-active').addClass('is-leaving');
+                    item.next().addClass('is-active');
+
+                    // focusing input
+                    $('input.form-control').focus();
+
+                    // show dropdown
+                    ddShow(field);
+
+                    if($('.modal-field--success').prev().hasClass('is-active')){
+                        $('#talk-form').addClass('form-last-input');
+                    }
+
+                    if($('.modal-field--success').hasClass('is-active')){
+                        $('#talk-form').addClass('form-completed');
+                    }
+                };
+
+                function ddShow(elem){
+                    elem.each(function(){
+                        var $this = $(this);
+
+                        if($this.hasClass('is-active') && $(e.target).parents(".custom-dropdown").length == 0){
+                            setTimeout(function(){
+                                $this.find('.dropdown-toggle').dropdown('toggle');
+                                $this.find('.custom-dropdown').addClass('show');
+                            }, 1000);
+
+                            dd($this);
+                        }
+                    });
+                };
+
+                function dd(el){
+                    el.find('.dropdown-menu .dropdown-item').click(function(e){
+                        var input = $(this).parents('.custom-dropdown').find('.dropdown-toggle');
+
+                        e.preventDefault();
+
+                        input.text($(this).text());
+
+                        setTimeout(function(){
+                            nextInput();
+                        }, 100);
+                    });
+                };
+            }
+
+        });
+
+        $('body').on('click', 'a#close-talk', function(e){
+            e.preventDefault();
+            hideTalkModal();
+        });
+    };
 
     return{
-        equalheight : equalheight,
-        ismobile    : ismobile,
+        equalheight      : equalheight,
+        ismobile         : ismobile,
 
-        ready       : ready,
-        sizing      : sizing,
+        ready            : ready,
+        sizing           : sizing,
 
-        fullHeight    : fullHeight,
-        minHeight     : minHeight,
-        sliderOptions : sliderOptions,
+        fullHeight       : fullHeight,
+        minHeight        : minHeight,
+        sliderOptions    : sliderOptions,
 
-        aboutInteraction : aboutInteraction
+        aboutInteraction : aboutInteraction,
+        talk             : talk
     }
 
 })();
